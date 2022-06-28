ccba846705bdb64ec00026d128f3b457111102a5
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-06-28 14:18:07 +0900

    Q1

---
+++ ex03/maze.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import tkinter.messagebox as ttk
+
+
+if __name__ ==  '__main__':
+    root = tk.Tk()
+    root.title("迷宫")
+    label = tk.Label(root,
+                     font=("Times New Roman", 80),
+                     text="你好"
+
+                     )
+    label.pack()
+    
+    root.mainloop()
