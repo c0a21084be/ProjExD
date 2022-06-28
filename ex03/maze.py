e01c3f4e4213ac9023727709402a752ed011f702
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-06-28 14:27:52 +0900

    Q3

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,11 +5,15 @@ import tkinter.messagebox as ttk
 if __name__ ==  '__main__':
     root = tk.Tk()
     root.title("迷宫")
-    label = tk.Label(root,
-                     font=("Times New Roman", 80),
-                     text="你好"
-
-                     )
-    label.pack()
+    canvas = tk.Canvas(root, 
+                       width = 1500, 
+                       height = 900, 
+                       bg = "black",
+                       )
+    canvas.pack()
+    tori = tk.PhotoImage(file="fig/5.png")
+    cx = 300
+    cy = 400
+    canvas.create_image(cx, cy, image=tori, tag="tori")
     
     root.mainloop()
