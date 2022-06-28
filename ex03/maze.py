980e407c63e534c1be21257b4498903c7390c641
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-06-28 14:54:12 +0900

    Q7

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,12 +4,24 @@ import tkinter.messagebox as ttk
 def key_down(event):
     global key
     key = event.keysym
-    print(f"[{key}]Clicked")
+    #print(f"[{key}]Clicked")
 
 def key_up(event):
     global key
     key = " "
 
+def main_proc():
+    global cx,cy,key
+    delta = { " ":[0,0],
+            "Up":[0,-20],
+             "Down":[0,+20], 
+             "Left":[-20,0], 
+             "Right":[+20,0],
+            
+    }
+    cx,cy = cx + delta[key][0], cy + delta[key][1]
+    canvas.coords("tori",cx,cy)
+    root.after(100,main_proc)
 
 if __name__ ==  '__main__':
     root = tk.Tk()
@@ -27,6 +39,7 @@ if __name__ ==  '__main__':
     
     key=" "
     root.bind("<KeyPress>",key_down)
-    root.bind("<KeyRelease>",key_down)    
-
+    root.bind("<KeyRelease>",key_up)    
+    
+    main_proc()
     root.mainloop()
