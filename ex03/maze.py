d7ec58933d6e368684a0cb5cd2c98c08b7d106ac
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-06-28 17:30:58 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,10 @@
+from operator import is_
+from sre_constants import SUCCESS
+from textwrap import fill
 import tkinter as tk
-import tkinter.messagebox as ttk
+import tkinter.messagebox as tkm
 import random
+from turtle import color
 
 def make_maze(yoko, tate):
     XP = [ 0, 1, 0, -1]
@@ -25,7 +29,7 @@ def make_maze(yoko, tate):
             maze_lst[y+YP[rnd]][x+XP[rnd]] = 1
 
     return maze_lst
-    
+
 def show_maze(canvas, maze_lst):
     color = ["white", "gray"]
     for y in range(len(maze_lst)):
@@ -33,25 +37,33 @@ def show_maze(canvas, maze_lst):
             canvas.create_rectangle(x*100, y*100, x*100+100, y*100+100, 
                                     fill=color[maze_lst[y][x]])
 
+
+   
 def key_down(event):
-    global key
+    global key, num,tori
     key = event.keysym
-    #print(f"[{key}]Clicked")
+    if key == "i":
+        num += 1
+        canvas.delete(kya)
+        tori = tk.PhotoImage(file=f"fig/{str(num)}.png")
+        canvas.create_image(cx, cy, image= tori, tag= "tori")
+        if num > 9:
+            num = 0
 
 def key_up(event):
     global key
     key = " "
 
 def main_proc():
-    global cx, cy, key, mx, my
-    delta = { " ":[0,0],
-            "Up":[0,-1],
+    global cx, cy, key, mx, my, is_goal
+    delta = {" ":[0,0],
+             "Up":[0,-1],
              "Down":[0,+1], 
              "Left":[-1,0], 
              "Right":[+1,0],
             
     }
-
+    
     try:
         if maze_bg[my+delta[key][1]][mx+delta[key][0]]==0:
             my,mx = my+delta[key][1],mx+delta[key][0]
@@ -60,11 +72,18 @@ def main_proc():
     #mx,my = mx + delta[key][0], my + delta[key][1]
     cx = mx*100+50
     cy = my*100+50
-
     canvas.coords("tori",cx,cy)
-    root.after(100,main_proc)
+    if cx ==gcx and cy == gcy:
+        is_goal = True
+        canvas.create_text(750,450,text="Success!!",font=("Times New Roman",100))
 
+    if not is_goal:
+        root.after(100,main_proc)
+
+
+   
 if __name__ ==  '__main__':
+    num = 0
     root = tk.Tk()
     root.title("迷宫")
     canvas = tk.Canvas(root, 
@@ -74,19 +93,31 @@ if __name__ ==  '__main__':
                        )
     canvas.pack()
 
+
     maze_bg = make_maze(15,9) 
     # print(maze_bg)
     show_maze(canvas, maze_bg)
-    tori = tk.PhotoImage(file="fig/5.png")
+    tori = tk.PhotoImage(file=f"fig/{str(num)}.png")
+    
     mx = 1
     my = 1
     cx = mx*100+50
     cy = my*100+50
-    canvas.create_image(cx, cy, image=tori, tag="tori")
-    
+
+    canvas.create_rectangle(cx+50,cy+50,cx-50,cy-50, fill="Green")
+    kya = canvas.create_image(cx, cy, image=tori, tag="tori")
+
+    goal = tk.PhotoImage(file="fig/6.png",)
+    gmx,gmy = 13 , 7
+    gcx,gcy = gmx*100+50, gmy*100+50
+    canvas.create_image(gcx, gcy, image=goal, tag="goal")
+
+    is_goal = False
+
     key=" "
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)    
+    
     
     main_proc()
     root.mainloop()
