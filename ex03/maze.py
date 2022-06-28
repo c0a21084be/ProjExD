1514ff182f0fb679341fcadd4b43692ac7e67bbb
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-06-28 15:44:10 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,8 +1,37 @@
-import imp
 import tkinter as tk
 import tkinter.messagebox as ttk
-import maze_maker as mm
+import random
 
+def make_maze(yoko, tate):
+    XP = [ 0, 1, 0, -1]
+    YP = [-1, 0, 1,  0]
+
+    maze_lst = []
+    for y in range(tate):
+        maze_lst.append([0]*yoko)
+    for x in range(yoko):
+        maze_lst[0][x] = 1
+        maze_lst[tate-1][x] = 1
+    for y in range(1, tate-1):
+        maze_lst[y][0] = 1
+        maze_lst[y][yoko-1] = 1
+    for y in range(2, tate-2, 2):
+        for x in range(2, yoko-2, 2):
+            maze_lst[y][x] = 1
+    for y in range(2, tate-2, 2):
+        for x in range(2, yoko-2, 2):
+            if x > 2: rnd = random.randint(0, 2)
+            else:     rnd = random.randint(0, 3)
+            maze_lst[y+YP[rnd]][x+XP[rnd]] = 1
+
+    return maze_lst
+    
+def show_maze(canvas, maze_lst):
+    color = ["white", "gray"]
+    for y in range(len(maze_lst)):
+        for x in range(len(maze_lst[y])):
+            canvas.create_rectangle(x*100, y*100, x*100+100, y*100+100, 
+                                    fill=color[maze_lst[y][x]])
 
 def key_down(event):
     global key
@@ -14,15 +43,24 @@ def key_up(event):
     key = " "
 
 def main_proc():
-    global cx,cy,key
+    global cx, cy, key, mx, my
     delta = { " ":[0,0],
-            "Up":[0,-20],
-             "Down":[0,+20], 
-             "Left":[-20,0], 
-             "Right":[+20,0],
+            "Up":[0,-1],
+             "Down":[0,+1], 
+             "Left":[-1,0], 
+             "Right":[+1,0],
             
     }
-    cx,cy = cx + delta[key][0], cy + delta[key][1]
+
+    try:
+        if maze_bg[my+delta[key][1]][mx+delta[key][0]]==0:
+            my,mx = my+delta[key][1],mx+delta[key][0]
+    except:
+        pass
+    #mx,my = mx + delta[key][0], my + delta[key][1]
+    cx = mx*100+50
+    cy = my*100+50
+
     canvas.coords("tori",cx,cy)
     root.after(100,main_proc)
 
@@ -36,12 +74,14 @@ if __name__ ==  '__main__':
                        )
     canvas.pack()
 
-    maze_bg = mm.make_maze(15,9) 
-    print(maze_bg)
-    mm.show_maze(canvas, maze_bg)
+    maze_bg = make_maze(15,9) 
+    # print(maze_bg)
+    show_maze(canvas, maze_bg)
     tori = tk.PhotoImage(file="fig/5.png")
-    cx = 300
-    cy = 400
+    mx = 1
+    my = 1
+    cx = mx*100+50
+    cy = my*100+50
     canvas.create_image(cx, cy, image=tori, tag="tori")
     
     key=" "
