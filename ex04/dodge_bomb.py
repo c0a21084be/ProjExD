f37a8c2688be767835ca91a79f028187a053c47c
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-07-05 15:48:53 +0900

    3限基本機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,7 @@
 import random
 import pygame as pg
 import sys
+import math
 
 def main():
 
@@ -14,21 +15,38 @@ def main():
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
     
     # practice 3
-    kkimg_sfc = pg.image.load("fig/6.png")
+    kkimg_sfc = pg.image.load(f"fig/6.png")
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
     kkimg_rct = kkimg_sfc.get_rect()
     kkimg_rct.center = 900,400
 
     #practice 5
-    bmimg_sfc = pg.Surface((20,20))
-    bmimg_sfc.set_colorkey((0,0,0))
-    pg.draw.circle(bmimg_sfc,(255,0,0), (10,10), 10)
-    bmimg_rct = bmimg_sfc.get_rect()
-    bmimg_rct.centerx = random.randint(0, screen_rct.width)
-    bmimg_rct.centery = random.randint(0, screen_rct.height)
-    vx, vy = +1, +1
+    bmimg_sfc_1 = pg.Surface((20,20))
+    bmimg_sfc_1.set_colorkey((0,0,0))
+    pg.draw.circle(bmimg_sfc_1,(255,0,0), (10,10), 10)
+    bmimg_rct_1 = bmimg_sfc_1.get_rect()
+    bmimg_rct_1.centerx = random.randint(0, screen_rct.width)
+    bmimg_rct_1.centery = random.randint(0, screen_rct.height)
+    vx1, vy1 = +1, +1
+
+    # bmimg_sfc_2 = pg.Surface((20,20))
+    # bmimg_sfc_2.set_colorkey((0,0,0))
+    # pg.draw.circle(bmimg_sfc_2,(255,0,0), (10,10), 10)
+    # bmimg_rct_2 = bmimg_sfc_2.get_rect()
+    # bmimg_rct_2.centerx = random.randint(0, screen_rct.width)
+    # bmimg_rct_2.centery = random.randint(0, screen_rct.height)
+    # vx2, vy2 = +2, +2
 
 
+    # bmimg_sfc_3 = pg.Surface((20,20))
+    # bmimg_sfc_3.set_colorkey((0,0,0))
+    # pg.draw.circle(bmimg_sfc_3,(255,0,0), (10,10), 10)
+    # bmimg_rct_3 = bmimg_sfc_3.get_rect()
+    # bmimg_rct_3.centerx = random.randint(0, screen_rct.width)
+    # bmimg_rct_3.centery = random.randint(0, screen_rct.height)
+    # vx3, vy3 = *math.pi, *math.pi
+
+    
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
         
@@ -38,18 +56,60 @@ def main():
        
         #practice 4
         key_states = pg.key.get_pressed()
-        if key_states[pg.K_UP]    == True: kkimg_rct.centery -= 1
-        if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
-        if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
-        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+        if key_states[pg.K_UP]       == True: kkimg_rct.centery -= 1
+        if key_states[pg.K_DOWN]     == True: kkimg_rct.centery += 1
+        if key_states[pg.K_LEFT]     == True: kkimg_rct.centerx -= 1
+        if key_states[pg.K_RIGHT]    == True: kkimg_rct.centerx += 1
+        if c_b(kkimg_rct,screen_rct) != (1, 1):
+            if key_states[pg.K_UP]       == True: kkimg_rct.centery += 1
+            if key_states[pg.K_DOWN]     == True: kkimg_rct.centery -= 1
+            if key_states[pg.K_LEFT]     == True: kkimg_rct.centerx += 1
+            if key_states[pg.K_RIGHT]    == True: kkimg_rct.centerx -= 1
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
-        bmimg_rct.move_ip(vx,vy)
-        screen_sfc.blit(bmimg_sfc, bmimg_rct)
+        bmimg_rct_1.move_ip(vx1,vy1)      
+        screen_sfc.blit(bmimg_sfc_1, bmimg_rct_1)
+
+        # bmimg_rct_2.move_ip(vx2,vy2)
+        # screen_sfc.blit(bmimg_sfc_2, bmimg_rct_2)
+
+        # bmimg_rct_3.move_ip(vx3,vy3)
+        # screen_sfc.blit(bmimg_sfc_3, bmimg_rct_3)
+       
+        #p7
+        w, h = c_b(bmimg_rct_1, screen_rct)
+        vx1 *= w
+        vy1 *= h
+       
+        # w, h = c_b(bmimg_rct_2, screen_rct)
+        # vx2 *= w
+        # vy2 *= h
+       
+        # w, h = c_b(bmimg_rct_3, screen_rct) 
+        # vx3 *= w
+        # vy3 *= h       
+        
+        #p8
+        if kkimg_rct.colliderect(bmimg_rct_1):
+            return
+
 
         pg.display.update()
         clock.tick(1000)
+
+def c_b(rct,scr_rct):
+
+    w, h = +1, +1
+
+    if rct.left < scr_rct.left or scr_rct.right  < rct.right:  w = -1
+    if rct.top  <  scr_rct.top or scr_rct.bottom < rct.bottom: h = -1
+
+    return w, h
+
+
 if __name__ == '__main__':  
+    
+    
     pg.init()
     main()
     pg.quit()
