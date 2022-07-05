334cdf8dd04698927fc1b8eefecb8547b03af9de
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-07-05 14:54:45 +0900

    p5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import pygame as pg
 import sys
 
@@ -12,11 +13,21 @@ def main():
     bgimg_rct  = bgimg_sfc.get_rect()
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
     
-    # pg.display.update()
+    # practice 3
     kkimg_sfc = pg.image.load("fig/6.png")
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
     kkimg_rct = kkimg_sfc.get_rect()
     kkimg_rct.center = 900,400
+
+    #practice 5
+    bmimg_sfc = pg.Surface((20,20))
+    bmimg_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bmimg_sfc,(255,0,0), (10,10), 10)
+    bmimg_rct = bmimg_sfc.get_rect()
+    bmimg_rct.centerx = random.randint(0, screen_rct.width)
+    bmimg_rct.centery = random.randint(0, screen_rct.height)
+    vx, vy = +1, +1
+
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
@@ -31,8 +42,11 @@ def main():
         if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
         if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
-        
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
+
+        
+        screen_sfc.blit(bmimg_sfc, bmimg_rct)
+
         pg.display.update()
         clock.tick(1000)
 if __name__ == '__main__':  
