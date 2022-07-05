29844e804d11d4711dd0764d6bcbda4f15d6bf22
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-07-05 14:36:01 +0900

    p4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -13,18 +13,28 @@ def main():
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
     
     # pg.display.update()
-    clock.tick(0.5)
+    kkimg_sfc = pg.image.load("fig/6.png")
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
+    kkimg_rct = kkimg_sfc.get_rect()
+    kkimg_rct.center = 900,400
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
-
+        
         #practice 2
         for event in pg.event.get():
             if event.type == pg.QUIT: return
        
+        #practice 4
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_UP]    == True: kkimg_rct.centery -= 1
+        if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
+        if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
+        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+        
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
         pg.display.update()
         clock.tick(1000)
-
 if __name__ == '__main__':  
     pg.init()
     main()
