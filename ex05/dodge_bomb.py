fb39978e7ab12c17a2d66cbf61173eb0d4b92f8b
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-07-12 15:11:53 +0900

    af3

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -9,7 +9,7 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900)) # Surface
     screen_rct = screen_sfc.get_rect()            # Rect
-    bgimg_sfc = pg.image.load("fig/pg_bg.jpg")    # Surface
+    bgimg_sfc = pg.image.load("pg_bg.jpg")    # Surface
     bgimg_rct = bgimg_sfc.get_rect()              # Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
