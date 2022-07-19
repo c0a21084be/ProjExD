4c3ad78acf50bf390fc77ea63d3163ab435bd126
C0A21084_Cho <c0a21084be@edu.teu.ac.jp>
2022-07-19 17:11:40 +0900

    変数初期化

--- ex06/game_stats.py
+++ ex06/game_stats.py
@@ -1,18 +1,14 @@
-class GameStats:
-    """跟踪游戏的统计信息"""
+class GameStats:        # ゲームの進行更新
 
-    def __init__(self, ai_game):
-        """初始化统计信息"""
+    def __init__(self, ai_game):        #初期化
         self.settings = ai_game.settings
         self.reset_stats()
 
-        # 游戏刚启动时处于活动状态。
-        self.game_active = False
-        # 任何情况下都不应重置最高得分
-        self.high_score = 0
+        self.game_active = False    #　ゲームが起動されたばかりのとき状態。
+        
+        self.high_score = 0         # 最大スコアをリセットしない
 
-    def reset_stats(self):
-        """初始化在游戏运行期间可能变化的统计信息"""
+    def reset_stats(self):      #初期化変数
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
